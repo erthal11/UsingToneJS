ef14458982d7d7050c8237129c1342d043d462e8
nathan <nathanerthal@live.com>
2020-09-26 13:53:21 -0400

    testing git

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -4,7 +4,7 @@
       <div class="container w-100">
         <div>
           <img class="waves" alt="waves" src="../assets/audioWaves.png">
-          <h1>Nathan Erthal</h1>
+          <h1>Nathan Erthal is gr8</h1>
           <img class="circular--landscape" alt="Nathan Erthal" src="../assets/Nathan&Kaiser.jpg">
           <h2> Software Engineer/ Audio Engineer</h2>
           <div>
