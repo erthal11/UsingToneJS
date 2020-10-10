e1bfdfeac4517f98e4e66bfddaa7e3fe3d9b4760
nathan <nathanerthal@live.com>
2020-10-09 20:52:54 -0400

    start of drum machine

--- src/components/Studio.vue
+++ src/components/Studio.vue
@@ -1,60 +1,79 @@
 <template>
   <div class="studio">
     <navbar :on_studio="true"></navbar>
-    <div class="container w-100 ">
+    <div class="container w-100">
+
+
+      <button @click="loopBeat">Play</button>
 
       <!--To Do:
       fix bug on hold keyboard note
+      make keys play on sliding from notes
       -->
       <button v-show="!onPiano" @click="onPiano=true">Synth</button>
       <button v-show="onPiano" @click="onPiano=false">Hide</button>
+
       <div v-show="onPiano">
-      <!--lower the octave -->
-      <button @click="octaveSwitch --" >-</button>
-      <!-- raise the octave-->
-      <button @click="octaveSwitch ++">+</button>
-      <!-- dropdown list to change wave shape (sine/triangle/saw/square) -->
-      <select v-model="synthShape" v-show="onPiano" name="shape" id="shapes">
-        <option @click="synthShape='sine'" value="sine">sine</option>
-        <option @click="synthShape='sine2'" value="sine2">sine2</option>
-        <option @click="synthShape='triangle'" value="triangle">triangle</option>
-        <option @click="synthShape='sawtooth'" value="sawtooth">saw</option>
-        <option @click="synthShape='square'" value="square">square</option>
-      </select>
+        <!--lower the octave -->
+        <button @click="octaveSwitch --" >-</button>
+        <!-- raise the octave-->
+        <button @click="octaveSwitch ++">+</button>
+        <!-- dropdown list to change wave shape (sine/triangle/saw/square) -->
+        <select v-model="synthShape" v-show="onPiano" name="shape" id="shapes">
+          <option @click="synthShape='sine'" value="sine">sine</option>
+          <option @click="synthShape='sine2'" value="sine2">sine2</option>
+          <option @click="synthShape='triangle'" value="triangle">triangle</option>
+          <option @click="synthShape='sawtooth'" value="sawtooth">saw</option>
+          <option @click="synthShape='square'" value="square">square</option>
+        </select>
 
-       <button @click="playSynthKeys(synthShape)">Use Keyboard</button>
+        <button @click="playSynthKeys(synthShape)">Use Keyboard</button>
 
-      <ul id="piano" v-show="onPiano" @mousedown="clickDown=true" @mouseup="clickDown=false">
+        <ul id="piano" v-show="onPiano" @mousedown="clickDown=true" @mouseup="clickDown=false">
 
-<!--  same keyboard hold note bug with @keydown and @keyup in ul tag -->
-<!--  <ul id="piano" v-show="onPiano" @mousedown="clickDown=true" @mouseup="clickDown=false"-->
-<!--  @keydown="playSynth('C4',synthShape,0)" @keyup="playSynth('C4',synthShape,1)"-->
-<!--  abindex="1">-->
+          <!--  same keyboard hold note bug with @keydown and @keyup in ul tag -->
+          <!--  <ul id="piano" v-show="onPiano" @mousedown="clickDown=true" @mouseup="clickDown=false"-->
+          <!--  @keydown="playSynth('C4',synthShape,0)" @keyup="playSynth('C4',synthShape,1)"-->
+          <!--  abindex="1">-->
 
-        <li @mousedown="playSynth('C4',synthShape,0)" @mouseup="playSynth('C4',synthShape,1)" class="key">A</li>
-        <li @mousedown="playSynth('C#4',synthShape,0)" @mouseup="playSynth('C#4',synthShape,1)" class="black-key">W</li>
-        <li @mousedown="playSynth('D4',synthShape,0)" @mouseup="playSynth('D4',synthShape,1)" class="key thing">S</li>
-        <li @mousedown="playSynth('D#4',synthShape,0)" @mouseup="playSynth('D#4',synthShape,1)" class="black-key">E</li>
-        <li @mousedown="playSynth('E4',synthShape,0)" @mouseup="playSynth('E4',synthShape,1)" class="key thing">D</li>
-        <li @mousedown="playSynth('F4',synthShape,0)" @mouseup="playSynth('F4',synthShape,1)" class="key ">F</li>
-        <li @mousedown="playSynth('F#4',synthShape,0)" @mouseup="playSynth('F#4',synthShape,1)" class="black-key">T</li>
-        <li @mousedown="playSynth('G4',synthShape,0)" @mouseup="playSynth('G4',synthShape,1)" class="key thing">G</li>
-        <li @mousedown="playSynth('G#4',synthShape,0)" @mouseup="playSynth('G#4',synthShape,1)" class="black-key">Y</li>
-        <li @mousedown="playSynth('A4',synthShape,0)" @mouseup="playSynth('A4',synthShape,1)" class="key thing">H</li>
-        <li @mousedown="playSynth('A#4',synthShape,0)" @mouseup="playSynth('A#4',synthShape,1)" class="black-key">U</li>
-        <li @mousedown="playSynth('B4',synthShape,0)" @mouseup="playSynth('B4',synthShape,1)" class="key thing">J</li>
-        <li @mousedown="playSynth('C5',synthShape,0)" @mouseup="playSynth('C5',synthShape,1)" class="key ">K</li>
-        <li @mousedown="playSynth('C#5',synthShape,0)" @mouseup="playSynth('C#5',synthShape,1)" class="black-key">O</li>
-        <li @mousedown="playSynth('D5',synthShape,0)" @mouseup="playSynth('D5',synthShape,1)" class="key thing">L</li>
-        <li @mousedown="playSynth('D#5',synthShape,0)" @mouseup="playSynth('D#5',synthShape,1)" class="black-key">P</li>
-        <li @mousedown="playSynth('E5',synthShape,0)" @mouseup="playSynth('E5',synthShape,1)" class="key thing">;</li>
-        <li @mousedown="playSynth('F5',synthShape,0)" @mouseup="playSynth('F5',synthShape,1)" class="key ">'</li>
-      </ul>
+          <li @mousedown="playSynth('C4',synthShape,0)" @mouseup="playSynth('C4',synthShape,1)" class="key">A</li>
+          <li @mousedown="playSynth('C#4',synthShape,0)" @mouseup="playSynth('C#4',synthShape,1)" class="black-key">W</li>
+          <li @mousedown="playSynth('D4',synthShape,0)" @mouseup="playSynth('D4',synthShape,1)" class="key thing">S</li>
+          <li @mousedown="playSynth('D#4',synthShape,0)" @mouseup="playSynth('D#4',synthShape,1)" class="black-key">E</li>
+          <li @mousedown="playSynth('E4',synthShape,0)" @mouseup="playSynth('E4',synthShape,1)" class="key thing">D</li>
+          <li @mousedown="playSynth('F4',synthShape,0)" @mouseup="playSynth('F4',synthShape,1)" class="key ">F</li>
+          <li @mousedown="playSynth('F#4',synthShape,0)" @mouseup="playSynth('F#4',synthShape,1)" class="black-key">T</li>
+          <li @mousedown="playSynth('G4',synthShape,0)" @mouseup="playSynth('G4',synthShape,1)" class="key thing">G</li>
+          <li @mousedown="playSynth('G#4',synthShape,0)" @mouseup="playSynth('G#4',synthShape,1)" class="black-key">Y</li>
+          <li @mousedown="playSynth('A4',synthShape,0)" @mouseup="playSynth('A4',synthShape,1)" class="key thing">H</li>
+          <li @mousedown="playSynth('A#4',synthShape,0)" @mouseup="playSynth('A#4',synthShape,1)" class="black-key">U</li>
+          <li @mousedown="playSynth('B4',synthShape,0)" @mouseup="playSynth('B4',synthShape,1)" class="key thing">J</li>
+          <li @mousedown="playSynth('C5',synthShape,0)" @mouseup="playSynth('C5',synthShape,1)" class="key ">K</li>
+          <li @mousedown="playSynth('C#5',synthShape,0)" @mouseup="playSynth('C#5',synthShape,1)" class="black-key">O</li>
+          <li @mousedown="playSynth('D5',synthShape,0)" @mouseup="playSynth('D5',synthShape,1)" class="key thing">L</li>
+          <li @mousedown="playSynth('D#5',synthShape,0)" @mouseup="playSynth('D#5',synthShape,1)" class="black-key">P</li>
+          <li @mousedown="playSynth('E5',synthShape,0)" @mouseup="playSynth('E5',synthShape,1)" class="key thing">;</li>
+          <li @mousedown="playSynth('F5',synthShape,0)" @mouseup="playSynth('F5',synthShape,1)" class="key ">'</li>
+        </ul>
+      </div>
+
+
+      <button v-show="!onDrums" @click="onDrums=true">Drums</button>
+      <button v-show="onDrums" @click="onDrums=false">Hide</button>
+
+      <div v-show="onDrums">
+
+        <ul>
+          <li @mousedown="loopBeat" class = drumPad >Kick</li>
+          <li class = drumPad >Snare</li>
+          <li class = drumPad >Hat</li>
+
+        </ul>
+
       </div>
 
 
     </div>
-
   </div>
 </template>
 
@@ -66,7 +85,7 @@ import * as Tone from 'tone'
 
 
 export default {
-name: "Studio",
+  name: "Studio",
 
   components: {
     Navbar
@@ -75,9 +94,11 @@ name: "Studio",
   data() {
     return {
       onPiano: false,
+      onDrums: false,
       synthShape: "sine",
       octaveSwitch: 0,
       synth: new Tone.Synth(),
+      bassSynth: new Tone.MembraneSynth(),
       clickDown: false,
       //keyRelease: false
     }
@@ -174,7 +195,7 @@ name: "Studio",
           case ";":
           case "'":
             this.playSynth("C4",shape,1)
-         }
+        }
 
       });
 
@@ -198,22 +219,38 @@ name: "Studio",
       // connect it to the master output (your speakers)
       this.synth.toDestination()
 
-      // trigger the attack immediately
+      // time 0 will trigger the note
       if (time===0) {
         this.synth.triggerAttack(note, now)
         console.log(note)
       }
 
+      // time 1 will release the note
       if (time===1) {
         this.synth.triggerRelease(now)
       }
 
       //attach a click listener to a play button
-      document.querySelector('button')?.addEventListener('click', async () => {
-        await Tone.start()
-      });
+      // document.querySelector('button')?.addEventListener('click', async () => {
+      //   await Tone.start()
+      // });
 
     },
+
+    loopBeat: function() {
+      Tone.start();
+
+      //interval: time interval for which song is updated (4n=quarter note)
+      const loopBeat = new Tone.Loop(this.song, '4n');
+      //transport is in charge of meter, bpm. (Its the thing that drives loop)
+      Tone.Transport.start();
+      loopBeat.start(0);
+    },
+
+    song: function(time) {
+      console.log(time)
+      this.bassSynth.triggerAttackRelease('c1', '8n', this.now)
+    }
 
   }
 
@@ -294,7 +331,7 @@ ul .key:active {
   box-shadow:-1px -1px 2px rgba(255,255,255,0.2) inset,0 -5px 2px 3px rgba(0,0,0,0.6) inset,0 2px 4px rgba(0,0,0,0.5);
   background:linear-gradient(45deg,#222 0%,#555 100%);
 
-  //z-index: 999;
+//z-index: 999;
   display: flex;
   justify-content: center;
   align-items: flex-end;
@@ -320,6 +357,25 @@ ul li:first-child {
 
 ul li:last-child {
   border-radius:0 5px 5px 5px
+}
+
+ul .drumPad {
+  height:6em;
+  width:6em;
+  z-index:1;
+  border: #222222;
+  //border-left:1px solid #222222;
+  //border-bottom:1px solid #222222;
+  border-radius: 5px 5px 5px 5px !important;
+  box-shadow:-1px 0 0 #555555 inset,0 0 5px #000000 inset,0 0 3px rgba(0,0,0,0.2);
+  background:linear-gradient(to bottom,#555555 0%,#444444 100%);
+  color: black;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  padding-bottom: 10px;
+  font-weight: bold;
+  margin-left: 15px;
 }
 
 
